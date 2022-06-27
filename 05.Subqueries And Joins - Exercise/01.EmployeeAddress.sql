13d74bc32a6aced42f455dd41021bd7b5c8898db
Valentin <85406438+valkata033@users.noreply.github.com>
2022-06-27 15:02:34 +0300

    Add files via upload

---
+++ 05.Subqueries And Joins - Exercise/01.EmployeeAddress.sql
@@ -0,0 +1,5 @@
+   SELECT TOP (5) [EmployeeID], [JobTitle], [e].[AddressID], [a].[AddressText]
+     FROM [Employees] AS [e]
+LEFT JOIN [Addresses] AS [a] 
+	   ON [e].[AddressID] = [a].[AddressID] 
+ ORDER BY [AddressID]
---
+++ 05.Subqueries And Joins - Exercise/02.AddressesWithTowns.sql
@@ -0,0 +1,5 @@
+ SELECT TOP (50) [FirstName], [LastName], [t].[Name], [a].[AddressText]
+	   FROM [Employees] AS [e]
+  LEFT JOIN [Addresses] AS [a] ON [e].[AddressID] = [a].[AddressID]
+  LEFT JOIN [Towns] AS [t] ON [a].[TownID] = [t].[TownID]
+   ORDER BY [FirstName], [LastName]
