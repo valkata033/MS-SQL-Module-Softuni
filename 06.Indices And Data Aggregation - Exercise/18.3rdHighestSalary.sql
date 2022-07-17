d9ff1d45ac19f1a99d4d9973aec9c4c0b09eecef
Valentin <85406438+valkata033@users.noreply.github.com>
2022-07-17 13:44:47 +0300

    Add files via upload

---
+++ 06.Indices And Data Aggregation - Exercise/18.3rdHighestSalary.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT [DepartmentID],
+				[Salary]
+  FROM ( 	
+		SELECT [DepartmentID],
+			   [Salary],
+			   DENSE_RANK() OVER (PARTITION BY [DepartmentID] ORDER BY [Salary] DESC)
+			AS [SalaryRank]
+		  FROM [Employees]
+	   ) AS [SalaryRankQuery]
+ WHERE [SalaryRank] = 3
